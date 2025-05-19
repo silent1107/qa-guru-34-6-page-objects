c678b15e2c3fc01fba71320d5cc5ec63928865f2
vladi <vladimirsiyamend@gmail.com>
2025-05-20 01:05:09 +0300

    refactored to Page Object pattern

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,16 +1,13 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import pages.PracticeFormPage;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selectors.byTagAndText;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.time.LocalDate;
 
-public class PracticeFormTests {
+public class PracticeFormTests extends BaseTest {
+
+    PracticeFormPage practiceFormPage = new PracticeFormPage();
 
     String firstName = "Ivan";
     String lastName = "Petrov";
@@ -27,50 +24,56 @@ public class PracticeFormTests {
     String address = "Random street, 103";
     String state = "Rajasthan";
     String city = "Jaiselmer";
-
-
-    @BeforeAll
-    static void setupEnvironment() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
-    }
+    LocalDate today = LocalDate.now();
+    String todayStr = today.getDayOfMonth() + " " + today.getMonth() + "," + today.getYear();
 
     @Test
     void fillStudentRegistrationFormTest() {
-        open("/automation-practice-form");
-        $("#firstName").setValue(firstName);
-        $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
-        $("input[value=" + gender + "]").ancestor(".custom-radio").click();
-        $("#userNumber").setValue(phoneNumber);
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").selectOption(year);
-        $(".react-datepicker__month-select").selectOption(month);
-        $(".react-datepicker__day--0" + day).click();
-        $("#subjectsContainer").click();
-        $("#subjectsInput").type(subject);
-        $("#react-select-2-option-0").click();
-        $("#hobbiesWrapper").$(byText(hobby1)).ancestor(".custom-checkbox").click();
-        $("#hobbiesWrapper").$(byText(hobby2)).ancestor(".custom-checkbox").click();
-        $("#uploadPicture").uploadFromClasspath(uploadedPicture);
-        $("#currentAddress").setValue(address);
-        $("#state").click();
-        $(byText(state)).click();
-        $("#city").click();
-        $(byText(city)).click();
-        $("#submit").click();
+        practiceFormPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setUserEmail(email)
+                .selectGender(gender)
+                .setUserNumber(phoneNumber)
+                .setDateOfBirth(year, month, day)
+                .selectSubject(subject)
+                .selectHobby(hobby1)
+                .selectHobby(hobby2)
+                .uploadPicture(uploadedPicture)
+                .setCurrentAddress(address)
+                .selectState(state)
+                .selectCity(city)
+                .clickSubmitButton()
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Student Email", email)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", phoneNumber)
+                .checkResult("Date of Birth", day + " " + month + "," + year)
+                .checkResult("Subjects", subject)
+                .checkResult("Hobbies", hobby1 + ", " + hobby2)
+                .checkResult("Picture", uploadedPicture)
+                .checkResult("Address", address)
+                .checkResult("State and City", state + " " + city);
+    }
 
+    @Test
+    void fillMinimumDataTest() {
+        practiceFormPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .selectGender(gender)
+                .setUserNumber(phoneNumber)
+                .clickSubmitButton()
+                .checkResult("Student Name", firstName + " " + lastName)
+                .checkResult("Gender", gender)
+                .checkResult("Mobile", phoneNumber)
+                .checkResult("Date of Birth", todayStr);
+    }
 
-        $(byTagAndText("td", "Student Name")).sibling(0).shouldHave(exactText(firstName + " " + lastName));
-        $(byTagAndText("td", "Student Email")).sibling(0).shouldHave(exactText(email));
-        $(byTagAndText("td", "Gender")).sibling(0).shouldHave(exactText(gender));
-        $(byTagAndText("td", "Mobile")).sibling(0).shouldHave(exactText(phoneNumber));
-        $(byTagAndText("td", "Date of Birth")).sibling(0).shouldHave(exactText(day + " " + month + "," + year));
-        $(byTagAndText("td", "Subjects")).sibling(0).shouldHave(exactText(subject));
-        $(byTagAndText("td", "Hobbies")).sibling(0).shouldHave(exactText(hobby1 + ", " + hobby2));
-        $(byTagAndText("td", "Picture")).sibling(0).shouldHave(exactText(uploadedPicture));
-        $(byTagAndText("td", "Address")).sibling(0).shouldHave(exactText(address));
-        $(byTagAndText("td", "State and City")).sibling(0).shouldHave(exactText(state + " " + city));
+    @Test
+    void fillWrongDataTest() {
+        practiceFormPage.openPage()
+                .clickSubmitButton()
+                .checkBordersColorInvalidInput();
     }
 }
